94d61a2ee4c7a512a93e9b5ce1ed7e84b7b97e8c
mattmckofke <matt.mckofke@gmail.com>
2025-01-29 10:13:12 -0500

    added render port

--- flask_api.py
+++ flask_api.py
@@ -45,4 +45,4 @@ def temperature_comparison():
     return send_file(buf, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
